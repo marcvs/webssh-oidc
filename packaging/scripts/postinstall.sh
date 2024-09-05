530351c5920339dccbfe2daa026ca8c424370b76
Marcus <marcus@hardt-it.de>
2024-09-05 10:10:50 +0200

    packaging definition

---
+++ packaging/scripts/postinstall.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -eu
+
+if command -v systemctl > /dev/null && [ "$(systemctl is-system-running)" != "offline" ]; then
+    systemctl daemon-reload
+fi
+systemctl start webssh-oidc.service
+
