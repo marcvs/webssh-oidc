3caa56fd5b34d2a92fef076e92f5c7ffe9715592
Marcus <marcus@hardt-it.de>
2024-09-05 14:13:38 +0200

    dont start the daemon in install script

--- packaging/scripts/postinstall.sh
+++ packaging/scripts/postinstall.sh
@@ -5,5 +5,5 @@ set -eu
 if command -v systemctl > /dev/null && [ "$(systemctl is-system-running)" != "offline" ]; then
     systemctl daemon-reload
 fi
-systemctl start webssh-oidc.service
+# systemctl start webssh-oidc.service
 
