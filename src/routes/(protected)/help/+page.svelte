efc3e7f70688e54e925d485fccee9f243997d34d
Marcus Hardt <marcus@hardt-it.de>
2026-03-31 13:33:51 +0200

    change content to focus on oinit

--- src/routes/(protected)/help/+page.svelte
+++ src/routes/(protected)/help/+page.svelte
@@ -7,6 +7,16 @@
 
 	export let data: PageData;
 
+	function downloadFile(content: string, filename: string) {
+		const blob = new Blob([content], { type: 'text/plain' });
+		const url = URL.createObjectURL(blob);
+		const a = document.createElement('a');
+		a.href = url;
+		a.download = filename;
+		a.click();
+		URL.revokeObjectURL(url);
+	}
+
 	const loginParams = {
 		user: data.session?.user.name ?? data.session?.user.email ?? data.session?.user.id ?? 'User',
 		accessToken: data.accessToken,
@@ -52,36 +62,141 @@
 		login to an SSH server with OpenId Connect using the commandline:
 	</p>
 
-	<h2 class="text-xl font-semibold text-mc-gray">1. Vanilla SSH</h2>
+
+	<h2 class="text-xl font-semibold text-mc-gray">1. ssh certificates</h2>
 
 	<p class="text-mc-gray">
-		You can use a vanilla SSH client to log in to the server. If you have already used the web
-		client, the user <span class="font-semibold">{loginParams.sshUser}</span> has already been created
-		for you. However, you will need to interactively pass the access token when prompted for it.
+		This tool uses SSH certificates signed by a CA instead of
+		traditional SSH keys. This provides short-lived credentials that are automatically issued based on
+		your OIDC identity.
 	</p>
-	<MyCodeArea label="SSH commandline:" id="sshCmd" value={sshCmd} />
 
-	<MyCodeArea label="Access Token:" id="token" value={loginParams.accessToken} />
+	{#if data.oinitPrivateKey && data.oinitCertificate}
+		<MyCodeArea
+			label="SSH command with certificate:"
+			id="oinitSshCmd"
+			open={true}
+			value={`ssh -i ~/.ssh/id_ed25519_${loginParams.sshHost.hostname} -p ${loginParams.sshHost.port} oinit@${loginParams.sshHost.hostname}`}
+		/>
 
-	<h2 class="text-xl font-semibold text-mc-gray">2. mccli</h2>
+		<!-- Access Token -->
+		<div class="flex items-center gap-4">
+			<button
+				on:click={() => downloadFile(loginParams.accessToken, 'bt_u1000')}
+				class="bg-mc-blue-500 hover:bg-mc-blue-600 text-white font-semibold py-2 px-4 rounded whitespace-nowrap"
+			>
+				Download Access Token
+			</button>
+			<span class="text-mc-gray text-xs">
+				Save to: <span class="font-mono">/tmp/bt_u$UID</span>
+			</span>
+			<div class="flex-1">
+				<MyCodeArea label="Access Token:" id="accessToken" value={data.accessToken} />
+			</div>
+		</div>
 
-	<p class="text-mc-gray">
-		This tool is a wrapper around the vanilla SSH client that makes sure that the local user is
-		created on the SSH server and then passes the access token to the server without any need for
-		user interaction. Mccli also supports integration with
-		<span class="font-semibold">oidc-agent</span>, a commandline tool for managing access tokens.
-		This will ensure that a valid access token is always available for the SSH client.
-	</p>
-	<MyCodeArea label="mccli command:" id="mccliCmd" value={mccliCmd} />
-	<MyCodeArea
-		label="mccli command with oidc-agent integration:"
-		id="mccliCmdOidcAgent"
-		value={mccliCmdOidcAgent}
-	/>
+		<!-- Private Key -->
+		<div class="flex items-center gap-4">
+			<button
+				on:click={() => downloadFile(data.oinitPrivateKey, `id_ed25519_${loginParams.sshHost.hostname}`)}
+				class="bg-mc-blue-500 hover:bg-mc-blue-600 text-white font-semibold py-2 px-4 rounded whitespace-nowrap"
+			>
+				Download Private Key
+			</button>
+			<span class="text-mc-gray text-xs">
+				Save to: <span class="font-mono">~/.ssh/id_ed25519</span><br/>
+				<span class="font-mono">chmod 600 ~/.ssh/id_ed25519</span>
+			</span>
+			<div class="flex-1">
+				<MyCodeArea label="Private Key:" id="oinitPrivateKey" value={data.oinitPrivateKey} />
+			</div>
+		</div>
 
+		<!-- Certificate -->
+		<div class="flex items-center gap-4">
+			<button
+				on:click={() => downloadFile(data.oinitCertificate, `id_ed25519_${loginParams.sshHost.hostname}-cert.pub`)}
+				class="bg-mc-blue-500 hover:bg-mc-blue-600 text-white font-semibold py-2 px-4 rounded whitespace-nowrap"
+			>
+				Download Certificate
+			</button>
+			<span class="text-mc-gray text-xs">
+				Save to: <span class="font-mono">~/.ssh/id_ed25519-cert.pub</span>
+			</span>
+			<div class="flex-1">
+				<MyCodeArea label="Certificate:" id="oinitCertificate" value={data.oinitCertificate} />
+			</div>
+		</div>
+
+	{:else}
+		<p class="text-mc-gray text-sm italic">
+			oinit is not configured for this server.
+		</p>
+	{/if}
+
+
+	<h2 class="text-xl font-semibold text-mc-gray">2. oinit</h2>
+
+    <p class="text-mc-gray">For easier access from the commandline, we
+    recommend to install the tools <span class="font-mono">oinit</span>
+    and <span class="font-mono">oidc-agent</span>:</p>
+	<ul class="list-disc list-inside text-mc-gray">
+		<li>
+            Install the corresponding package repository for your linux
+            distribution. Details at:
+			<a
+				href="https://repo.data.kit.edu"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="text-mc-blue-500 underline"
+			>
+				https://repo.data.kit.edu
+			</a>
+		</li>
+		<li>
+            Install packages:<br/>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="font-mono">yum -y install oinit oidc-agent</span><br/>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="font-mono">apt -y install oinit oidc-agent</span>
+        </li>
+        <li>
+        Log in:
+		<MyCodeArea
+			label="SSH commands with oinit:"
+			id="oinitSshCmd"
+			open={true}
+			value={`oinit add ${loginParams.sshHost.hostname}       # Tell oinit that ${loginParams.sshHost.hosname} is under oinit control (only needed once)
+ssh -p ${loginParams.sshHost.port} ${loginParams.sshHost.hostname}`}
+		/>
+        </li>
+        <li>
+            Learn more at
+			<a
+				href="https://ssh-oidc-doc.data.kit.edu"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="text-mc-blue-500 underline"
+			>
+				https://ssh-oidc-doc.data.kit.edu
+			</a>
+        </li>
+    </ul>
+
+
+    <hr/>
 	<h1 class="text-2xl font-semibold text-mc-gray">Resources</h1>
 	<p class="text-mc-gray">Here are some resources to help you get started with SSH-OIDC.</p>
 	<ul class="list-disc list-inside text-mc-gray">
+        <li>
+            The documentation of ssh-oidc
+			<a
+				href="https://ssh-oidc-doc.data.kit.edu"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="text-mc-blue-500 underline"
+			>
+				ssh-oidc-doc
+			</a>
+        </li>
 		<li>
 			The web client for ssh:
 			<a
