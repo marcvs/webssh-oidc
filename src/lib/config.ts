feda61557f22f1474dde864a03360ad706ad9bc6
Marcus Hardt <marcus@hardt-it.de>
2026-03-31 10:13:28 +0200

    add PUBLIC_OINIT_ENDOPINT_URL

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,6 +2,7 @@ import { env } from '$env/dynamic/public';
 
 const CONFIG = {
 	mcEndpoint: env.PUBLIC_MC_ENDPOINT_URL,
+	oinitEndpoint: env.PUBLIC_OINIT_ENDPOINT_URL,
 	sshHost: {
 		hostname: env.PUBLIC_SSH_HOSTNAME_FQDN,
 		port: parseInt(env.PUBLIC_SSH_PORT) || 22
